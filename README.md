b0f971be5e56df00bde24d381d12335b0594e4ab
gmazy <gmazy@runbox.com>
2026-08-27 19:53:48 +0300

    Version 2026.2.3
    - Camera already in ortographic will keep it's angle
    - Proper unit limits
    - Scale option in preferences
    - Fixed slightly incorrect DPI values

--- README.md
+++ README.md
@@ -1,24 +1,25 @@
 <h1 align="center">Real Size Tools for Blender</h1>
-<p align="center"><i>3D printing focused add-on to help view models and images in their actual size</i></p>
+<p align="center"><i>Add-on to help view models and images in their actual real life size</i></p>
+
+
+## How it works
+
+### Viewport in Actual Size (Wip)
+1. Click on `View` > `Viewpoint` > `View in Actual Size`
+2. Expand box in bottom left corner of screen and set `Screen DPI` to value matching your screen.
+3. Blender view dimensions should now match real life. Verify it using physical ruler on screen.
+
+### Background Image in Actual Size
+1. Drag and drop image file with correctly set metadata into Blender, for example image from flatbed scanner.
+2. Blender will create empty with the image. Go to properties of empty and click on `View in Actual Size`
+
+### Image Editor in Actual Size
+1. Open image in image editor and and click on `View` > `Zoom` > `View in Actual Size`
+2. If size is not correct, double check screen DPI in add-on preferences.
 <br>
 
-## 📥 How to Install
+## 📥 Install
 Download the master branch as a .zip file. 
 
 In Blender go to `Edit` > `Preferences` > `Add-ons`. Drag and drop the .zip file into the window to install.
 
-<br>
-
-# Examples:
-
-## Viewport in Actual Size (Wip)
-1. Click on `View` > `Viewpoint` > `View in Actual Size`
-2. Expand box in bottom left corner of screen and set `Screen DPI` to value matching your screen.
-3. Blender view dimensions should now match real life. Verify it using physical ruler on screen.
-<br>
-
-## Background Image in Actual Size
-1. Drag and drop image file with correctly set metadata into Blender, for example image from flatbed scanner.
-2. Blender will create Empty with the image. Go to properties of Empty and click on `View in Actual Size`
-3. Image should have now correct scale. Verify it using ruler tool.
-<br>
--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {  
     "name": "Real Size Tools",  
     "author": "Mazay",  
-    "version": (0, 1, 0),
+    "version": (2026, 2, 3),
     "blender": (2, 80, 0),  
     "location": "View > Viewpoint, Empty > Image",  
     "description": "Set viewport to actual real life scale, load reference images in scale.",  
@@ -20,45 +20,68 @@ class real_size_tools(bpy.types.AddonPreferences):
 
     screen_dpi : FloatProperty(
         name="Screen DPI",
-        description="\nType your display DPI from display manual",
+        description="Type your display DPI from display manual",
         default=95,
+        soft_min=1,
         )
 
     units_are_mm : BoolProperty(
-        name="Treat default unit as millimeter",
-        description="1000x scale will be applied when this is enabled and "
-                    "Blender's Unit Scale is left at it's default value.\n"
-                    "Note: Blender restart may be required for view scale change.",
+        name="Use scale instead of real size",
+        description="Scale will be applied on top of real size when this is enabled\n"
+                    "and Blender's Unit Scale is left at it's default value",
         default=False,
+        )
+
+    extra_scale : FloatProperty(
+        name="Scale 1:x",
+        description="Scale 1:x",
+        default=1000,
+        soft_min=1,
         )
 
     def draw(self, context):
         layout = self.layout
         layout.prop(self, "screen_dpi")
         layout.separator()
-        layout.label(text="3D Printing Specials:")
         layout.prop(self, "units_are_mm")
+        if self.units_are_mm:
+            layout.prop(self, "extra_scale")
+            if self.extra_scale != 1:
+                scale_format = prefs.scale_str()
+                if bpy.context.scene.unit_settings.scale_length != 1: #
+                    layout.label(text="Scale disabled, please set Blender's unit scale to 1")
+                else:
+                    layout.label(text="This enables view scale of 1:"+scale_format+
+                                      " and image/empty scale of "+scale_format)
+
 
 class RealSizeView(bpy.types.Operator):
-    """View in real size using display DPI from Real Size Tools addon's preferences"""
+    """View in real size"""
     bl_idname = "view3d.real_size_view"
     bl_label = "View in Actual Size"
     bl_options = {'REGISTER', 'UNDO'}
 
     screen_dpi : FloatProperty(
         name="Screen DPI",
-        description="Screen DPI for current operation. ",
-        default=0)
+        description="Screen DPI for current operation",
+        default=0,
+        soft_min=0,
+        step=10,
+        options={'SKIP_SAVE'})
     save_screen_dpi : BoolProperty(
         name="Set as default",
         description="Save DPI as the new default changing addon preferences",
-        default=False)
+        default=False,
+        options={'SKIP_SAVE'})
     scale : FloatProperty(
         name="Scale 1:x",
-        description="Set value of this to 2 to use 1:2 scale.",
-        precision=0,
-        default=0)
-
+        description="Set value of this to 2 to use 1:2 scale",
+        default=0,
+        soft_min=1,
+        soft_max=1000,
+        step=10,
+        precision=1,
+        options={'SKIP_SAVE'})
 
     def execute(self, context):
         preferences = bpy.context.preferences.addons[__name__].preferences
@@ -69,61 +92,112 @@ class RealSizeView(bpy.types.Operator):
             self.screen_dpi = preferences.screen_dpi
 
         # Setting Scale to 0 uses default scale.
-        if self.scale == 0 and preferences.units_are_mm and unit_scale == 1:
-            self.scale = 1000
-        elif self.scale == 0:
-            self.scale = 1
+        if self.scale == 0:
+            self.scale = prefs.scale()
 
-        # Update addon preferences with enabled checkbox
+        # Update addon preferences with checked checkbox
         if self.save_screen_dpi:
             preferences.screen_dpi = self.screen_dpi
 
         area = next(area for area in bpy.context.screen.areas if area.type == "VIEW_3D")
-        with bpy.context.temp_override(area=area, region=area.regions[-1]):
-            bpy.ops.view3d.view_axis(type='FRONT')     
-        region_3d = area.spaces.active.region_3d
 
-        region_3d.view_distance = area.width / self.screen_dpi * 0.01753 * self.scale / unit_scale
+        # Switch to ortographic
+        region_3d = area.spaces.active.region_3d
+        if region_3d.view_perspective == 'PERSP':
+            with bpy.context.temp_override(area=area, region=area.regions[-1]):
+                bpy.ops.view3d.view_axis(type='FRONT')   
+
+        # Set view in DPI
+        screen_dpm = self.screen_dpi / 25.4 * 1000 # Pixels per screen m
+        scene_dpm = area.width / 1.44 # Pixels per scene m
+        region_3d.view_distance = scene_dpm / screen_dpm / unit_scale * self.scale
         return {'FINISHED'}
 
-class RealSizeImage(bpy.types.Operator):
+class prefs():
+    def scale():
+        unit_scale = bpy.context.scene.unit_settings.scale_length
+        preferences = bpy.context.preferences.addons[__name__].preferences
+        if preferences.units_are_mm and unit_scale == 1:
+            return preferences.extra_scale
+        return 1
+
+    @classmethod
+    def scale_xyz(cls):
+        scale = cls.scale()
+        return (scale,scale,scale)
+
+    @classmethod
+    def scale_str(cls):
+        scale = str(round(cls.scale(),3))
+        return scale.rstrip('0').rstrip('.') if '.' in scale else scale
+
+class RealSizeImageEmpty(bpy.types.Operator):
     """Set image to it's actual size based on image metadata"""
-    bl_idname = "view3d.real_size_image" #VIEW3D_OT_real_size_image
+    bl_idname = "view3d.real_size_image_empty" #VIEW3D_OT_real_size_image_viewer
     bl_label = "View in Actual Size"
     bl_options = {'REGISTER', 'UNDO'}
 
     @classmethod
     def calc_display_size(cls, img):
         unit_scale = bpy.context.scene.unit_settings.scale_length
-        units_are_mm = bpy.context.preferences.addons[__name__].preferences.units_are_mm
         display_size = max(img.size) / max(img.resolution) / unit_scale
-        if unit_scale == 1 and units_are_mm:
-            display_size = display_size / 0.001
-        return display_size
+        return display_size * prefs.scale()
 
     @classmethod
     def poll(cls, context):
         obj = context.object
         if obj.type == 'EMPTY' and obj.empty_display_type == 'IMAGE':
+            scale = prefs.scale()
             display_size = cls.calc_display_size(obj.data)
-            if numpy.float32(obj.empty_display_size) != numpy.float32(display_size):
+            if (numpy.float32(obj.empty_display_size) != numpy.float32(display_size) or
+                tuple(obj.scale) != scale):
                 return True
         return False
 
     def execute(self, context):
         obj = context.object
         obj.empty_display_size = self.calc_display_size(obj.data)
+        obj.scale = prefs.scale_xyz()
         return {'FINISHED'}
+
+class RealSizeImageViewer(bpy.types.Operator):
+    """Set image to it's actual size based on image metadata"""
+    bl_idname = "image.view_zoom_real_size" 
+    bl_label = "View in Actual Size"
+
+    @classmethod
+    def poll(cls, context):
+        return True
+
+    def execute(self, context):
+        # Find open image ditor. This may fail if multiple editors are open.
+        area = False
+        for area in bpy.context.screen.areas :
+            if area.type == 'IMAGE_EDITOR' or area.type == 'UV_EDITOR':
+                break
+        # Find image
+        if area and area.spaces.active.image:
+            img = area.spaces.active.image
+
+            # Find dpi ratio
+            image_dpi = max(img.resolution) * 0.0254
+            screen_dpi = bpy.context.preferences.addons[__name__].preferences.screen_dpi
+            ratio = screen_dpi / image_dpi
+
+            # Change view zoom
+            bpy.ops.image.view_zoom_ratio(ratio=ratio)
+        return {'FINISHED'}
+
 
 def menu_func_view(self, context):
     self.layout.separator()
     op_text = "View in Actual Size"
-    if (context.preferences.addons[__name__].preferences.units_are_mm and
-        context.scene.unit_settings.scale_length == 1):
-        op_text += " (1:1000)"
+    scale = prefs.scale_str()
+    if scale != "1":
+        op_text += " (1:" + scale + ")"
     self.layout.operator(RealSizeView.bl_idname, text=op_text)
 
-def menu_func_image(self, context):
+def menu_func_image_empty(self, context):
     obj = context.object
     if obj.type == 'EMPTY' and obj.empty_display_type == 'IMAGE':
         dpi = max(obj.data.size) / obj.empty_display_size * 0.0254 / context.scene.unit_settings.scale_length
@@ -132,27 +206,35 @@ def menu_func_image(self, context):
         layout.label(text="DPI: "+str(round(dpi,2)))
 
         op_text = "View in Actual Size"
-        if (context.preferences.addons[__name__].preferences.units_are_mm and
-            context.scene.unit_settings.scale_length == 1):
-            op_text += " x 1000"
-        layout.operator(RealSizeImage.bl_idname, text=op_text)
+        scale = prefs.scale_str()
+        if scale != "1":
+            op_text += " x " + scale
+        layout.operator(RealSizeImageEmpty.bl_idname, text=op_text)
 
+def menu_func_image_viewer(self, context):
+    obj = context.object
+    self.layout.separator()
+    self.layout.operator(RealSizeImageViewer.bl_idname)
 
 
 def register():
     bpy.utils.register_class(RealSizeView)
-    bpy.utils.register_class(RealSizeImage)
+    bpy.utils.register_class(RealSizeImageEmpty)
+    bpy.utils.register_class(RealSizeImageViewer)
     bpy.utils.register_class(real_size_tools)
     bpy.types.VIEW3D_MT_view_viewpoint.append(menu_func_view)
-    bpy.types.DATA_PT_empty.append(menu_func_image)
+    bpy.types.DATA_PT_empty.append(menu_func_image_empty)
+    bpy.types.IMAGE_MT_view_zoom.append(menu_func_image_viewer)
 
 
 def unregister():
     bpy.utils.unregister_class(RealSizeView)
-    bpy.utils.unregister_class(RealSizeImage)
+    bpy.utils.unregister_class(RealSizeImageEmpty)
+    bpy.utils.unregister_class(RealSizeImageViewer)
     bpy.utils.unregister_class(real_size_tools)
     bpy.types.VIEW3D_MT_view_viewpoint.remove(menu_func_view)
-    bpy.types.DATA_PT_empty.remove(menu_func_image)
+    bpy.types.DATA_PT_empty.remove(menu_func_image_empty)
+    bpy.types.IMAGE_MT_view_zoom.remove(menu_func_image_viewer)
 
 
 if __name__ == "__main__":
