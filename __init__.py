908d9e8792ce9904bc1955c63b38758c4a56ea7d
gmazy <gmazy@runbox.com>
2026-08-27 10:00:44 +0300

    DPI calibration in fly

--- __init__.py
+++ __init__.py
@@ -1,14 +1,14 @@
 bl_info = {  
     "name": "Real Size Tools",  
     "author": "Mazay",  
-    "version": (0, 0, 1),  
+    "version": (0, 1, 0),
     "blender": (2, 80, 0),  
     "location": "View > Viewpoint, Empty > Image",  
     "description": "Set viewport to actual real life scale, load reference images in scale.",  
     "warning": "",  
-    "wiki_url": "",  
-    "tracker_url": "",  
-    "category": "Other"}
+    "doc_url": "https://github.com/gmazy/blender-real-size-tools",  
+    "tracker_url": "https://github.com/gmazy/blender-real-size-tools/issues",  
+    "category": "3D View"}
 
 import bpy
 import math
@@ -19,14 +19,16 @@ class real_size_tools(bpy.types.AddonPreferences):
     bl_idname = __name__
 
     screen_dpi : FloatProperty(
-            name="Screen DPI",
-            description="\nType your display DPI from display manual",
-            default=95, #95 for desktop 157.35 for laptop
-            )
+        name="Screen DPI",
+        description="\nType your display DPI from display manual",
+        default=95,
+        )
 
     units_are_mm : BoolProperty(
-        name="I believe all units are millimeters",
-        description="1000x scale will be applied when this is enabled and Blender's Unit Scale is left at it's default value",
+        name="Treat default unit as millimeter",
+        description="1000x scale will be applied when this is enabled and "
+                    "Blender's Unit Scale is left at it's default value.\n"
+                    "Note: Blender restart may be required for view scale change.",
         default=False,
         )
 
@@ -34,7 +36,7 @@ class real_size_tools(bpy.types.AddonPreferences):
         layout = self.layout
         layout.prop(self, "screen_dpi")
         layout.separator()
-        layout.label(text="3D Printing specials:")
+        layout.label(text="3D Printing Specials:")
         layout.prop(self, "units_are_mm")
 
 class RealSizeView(bpy.types.Operator):
@@ -43,18 +45,45 @@ class RealSizeView(bpy.types.Operator):
     bl_label = "View in Actual Size"
     bl_options = {'REGISTER', 'UNDO'}
 
+    screen_dpi : FloatProperty(
+        name="Screen DPI",
+        description="Screen DPI for current operation. ",
+        default=0)
+    save_screen_dpi : BoolProperty(
+        name="Set as default",
+        description="Save DPI as the new default changing addon preferences",
+        default=False)
+    scale : FloatProperty(
+        name="Scale 1:x",
+        description="Set value of this to 2 to use 1:2 scale.",
+        precision=0,
+        default=0)
+
+
     def execute(self, context):
+        preferences = bpy.context.preferences.addons[__name__].preferences
+        unit_scale = context.scene.unit_settings.scale_length
+
+         # Setting DPI to 0 uses value from preferences.
+        if self.screen_dpi == 0:
+            self.screen_dpi = preferences.screen_dpi
+
+        # Setting Scale to 0 uses default scale.
+        if self.scale == 0 and preferences.units_are_mm and unit_scale == 1:
+            self.scale = 1000
+        elif self.scale == 0:
+            self.scale = 1
+
+        # Update addon preferences with enabled checkbox
+        if self.save_screen_dpi:
+            preferences.screen_dpi = self.screen_dpi
+
         area = next(area for area in bpy.context.screen.areas if area.type == "VIEW_3D")
         with bpy.context.temp_override(area=area, region=area.regions[-1]):
             bpy.ops.view3d.view_axis(type='FRONT')     
         region_3d = area.spaces.active.region_3d
 
-        display_dpi = bpy.context.preferences.addons[__name__].preferences.screen_dpi
-        unit_scale = context.scene.unit_settings.scale_length
-        scale = 1
-        if context.preferences.addons[__name__].preferences.units_are_mm and unit_scale==1:
-            scale = 1000
-        region_3d.view_distance = area.width / display_dpi * 0.01753 * scale / unit_scale
+        region_3d.view_distance = area.width / self.screen_dpi * 0.01753 * self.scale / unit_scale
         return {'FINISHED'}
 
 class RealSizeImage(bpy.types.Operator):
@@ -89,7 +118,8 @@ class RealSizeImage(bpy.types.Operator):
 def menu_func_view(self, context):
     self.layout.separator()
     op_text = "View in Actual Size"
-    if context.preferences.addons[__name__].preferences.units_are_mm and context.scene.unit_settings.scale_length == 1:
+    if (context.preferences.addons[__name__].preferences.units_are_mm and
+        context.scene.unit_settings.scale_length == 1):
         op_text += " (1:1000)"
     self.layout.operator(RealSizeView.bl_idname, text=op_text)
 
@@ -102,7 +132,8 @@ def menu_func_image(self, context):
         layout.label(text="DPI: "+str(round(dpi,2)))
 
         op_text = "View in Actual Size"
-        if context.preferences.addons[__name__].preferences.units_are_mm and context.scene.unit_settings.scale_length == 1:
+        if (context.preferences.addons[__name__].preferences.units_are_mm and
+            context.scene.unit_settings.scale_length == 1):
             op_text += " x 1000"
         layout.operator(RealSizeImage.bl_idname, text=op_text)
 
